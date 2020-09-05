3c1f6466a7367363bdf821b3dd57ded5ef46c192
Darczi <Radoslawpunda@gmail.com>
2020-09-05 21:47:17 +0200

    Zmiany

---
+++ MiniShop/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop
+{
+    enum OrderStatus
+    {
+        NewOrder =10,
+        Complet = 20,
+        Confirmed = 30,
+        Shipped = 40,
+        Returned = 50,
+        Cancelled = 60
+    }
+    class Order
+    {
+        private string orderNumber;
+        private string customerName;
+        private string shipAddress;
+        private DateTime orderDate;
+        private OrderStatus status = OrderStatus.NewOrder;
+        private byte discount = 0;
+        private List<OrderItem> items = new List<OrderItem>();
+    }
+}
---
+++ MiniShop/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace MiniShop
+{
+    public class Product
+    {
+        private int id;
+        private string name;
+        private double price;
+        private string descr = string.Empty;
+
+        private bool promo = false;
+        private bool active = true;
+
+        public Product(int id, string name, double price, string descr="")
+        {
+            this.id = id; this.name = name; this.price = price; this.descr = descr;
+        }
+    }
+}
